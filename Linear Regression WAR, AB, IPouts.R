db37e1dfcb0f5f026d616d9fb6be0486749e0c2f
jazippay <61477492+jazippay@users.noreply.github.com>
2020-03-26 16:48:38 -0700

    Add files via upload

--- Linear Regression WAR, AB, IPouts.R
+++ Linear Regression WAR, AB, IPouts.R
@@ -1,41 +1,41 @@
-#Linear Regression Analysis WAR and AB, WAR and IPouts
-#Create a Dataframe of WAR and AB by combining warBatNA and batting
+#Linear Regression Analysis WAR and PA, WAR and IPouts
+#PA = Plate Attempts, WAR = Wins Above Average, IPouts
+#Create a Dataframe of WAR and PA by combining warBatNA and batting
 #Create a Dataframe of WAR and IPouts from warPitchNA
 
-#Dataframe merging WAR and AB variables      
-keepA <- c('yearID', 'AB', 'playerID')
-keepB <- c('WAR', 'team_ID', 'playerID')
-BattingA <- Batting[keepA]
-warB <- warBatNA[keepB]
+#Dataframe merging WAR and AB variables, this is for AB changed to PA      
+#keepA <- c('yearID', 'AB', 'playerID')
+#keepB <- c('WAR', 'team_ID', 'playerID')
+#BattingA <- Batting[keepA]
+#warB <- warBatNA[keepB]
 
-warBatting <- merge(BattingA,warB,by=c("playerID"))
-View(warBatting)
+#warBatting <- merge(BattingA,warB,by=c("playerID"))
+#View(warBatting)
 
 #Create 3 Different dataframes based on range of years; 95-2019,95-99 and 2015-2019
-warBattingAll <- warBatting[which(warBatting$yearID > 1994 & warBatting$yearID < 2020), ]
-warBatting90 <- warBatting[which(warBatting$yearID > 1994 & warBatting$yearID < 2000), ]
-warBatting2000 <- warBatting[which(warBatting$yearID > 2014 & warBatting$yearID < 2020), ]
+warBattingAll <- warBatNA[which(warBatNA$year_ID > 1994 & warBatNA$year_ID < 2020), ]
+warBatting90 <- warBatNA[which(warBatNA$year_ID > 1994 & warBatNA$year_ID < 2000), ]
+warBatting2000 <- warBatNA[which(warBatNA$year_ID > 2014 & warBatNA$year_ID < 2020), ]
 
 #Remove NA from all datasets
-warBattingAll <- na.omit(warBattingAll)
+#warBattingAll <- na.omit(warBattingAll)
 #Conver WAR to numeric
-warBatingAll$WAR <- as.numeric(warBatingAll$WAR)
+#warBatingAll$WAR <- as.numeric(warBatingAll$WAR)
 
 #linear model formula
-lin_reg <- lm(AB ~ WAR, warBattingAll)
+lin_reg <- lm(PA ~ WAR, warBattingAll)
 summary(lin_reg)
-#Adjusted R-sqaured .23
+#Adjusted R-sqaured .52
 #significant p value
-lin_reg <- lm(AB ~ WAR, warBatting90)
+lin_reg <- lm(PA ~ WAR, warBatting90)
 summary(lin_reg)
-#Adjusted R-sqaured .21
+#Adjusted R-sqaured .51
 #significant p value
-lin_reg <- lm(AB ~ WAR, warBatting2000)
+lin_reg <- lm(PA ~ WAR, warBatting2000)
 summary(lin_reg)
-#Adjusted R-sqaured .30
+#Adjusted R-sqaured .53
 #significant p value
-#therefore At bats (AB) tells us about 20% or 30% in the modern era of the factors that go into WAR
-
+#therefore Plate Attempts tells about 50% of the factors that go into WAR, pretty high!
 
 #Create a Dataframe of WAR and IPouts from warPitchNA
 #Create 3 Different dataframes based on range of years; 95-2019,95-99 and 2015-2019
@@ -57,8 +57,10 @@ summary(lin_reg)
 #Adjusted R-sqaured .39
 #significant p value
 #therefore, IPouts determine atleast 40% of the factors that go into WAR, not too shabby
+
 #final conclusion there must be some other factors that determine WAR in both pitching and batting. 
 #But IPouts explain the variability of WAR in pitching more so than AB explains WAR in batting
+#When using Plate Attempts we find 50% of WAR explained in WAR batting stats. 
 
 #Plot of linear models
 library(ggplot2)
@@ -68,3 +70,6 @@ d + geom_point() + geom_smooth(method=lm, se=FALSE)
 ggplot(warBatting2000, aes(WAR, AB)) +
   geom_point() +
   geom_smooth(method = "lm")
+
+d <- ggplot(warBatting2000, aes(WAR, PA))
+d + geom_point() + geom_smooth(method = "lm", se=FALSE)
